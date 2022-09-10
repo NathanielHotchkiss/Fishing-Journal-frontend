18d3183a90c101ce403479fea53015e8cf899cc0
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-09-10 11:02:26 -0400

    margin and padding adjustments

--- src/components/ErrorAlert.jsx
+++ src/components/ErrorAlert.jsx
@@ -3,7 +3,7 @@ import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
 export default function ErrorAlert({ error }) {
   return (
     error && (
-      <div className="rounded-md bg-red-50 p-4">
+      <div className="rounded-md bg-red-50 px-4 py-2 my-2">
         <div className="flex">
           <div className="flex-shrink-0">
             <ExclamationCircleIcon
