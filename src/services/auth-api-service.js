abdf91cf6b007c82ddb334f66a62bb0e8efd29cc
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-09-15 16:37:13 -0400

    crud for users

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -2,17 +2,6 @@ import config from "../config";
 import TokenService from "./token-service";
 
 const AuthApiService = {
-  postUser(user) {
-    return fetch(`${config.API_ENDPOINT}/app_users/new`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(user),
-    }).then((res) =>
-      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
-    );
-  },
   postLogin({ email, password }) {
     return fetch(`${config.API_ENDPOINT}/auth/token`, {
       method: "POST",
@@ -34,8 +23,8 @@ const AuthApiService = {
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
-  postNewLog(user) {
-    return fetch(`${config.API_ENDPOINT}/fishing_logs`, {
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/app_users/new`, {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -45,10 +34,52 @@ const AuthApiService = {
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
-  updateLog(fish) {
-    return fetch(`${config.API_ENDPOINT}/fishing_logs/:fish_id`, {
+  updateUser(user) {
+    const { user_id } = user;
+    return fetch(`${config.API_ENDPOINT}/app_users/${user_id}`, {
       method: "PUT",
       headers: {
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  deleteUser(user_id) {
+    return fetch(`${config.API_ENDPOINT}/app_users/${user_id}`, {
+      method: "DELETE",
+      headers: {
+        user_id,
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (!res.ok) {
+        return res.json().then((e) => Promise.reject(e));
+      }
+      return res;
+    });
+  },
+  postNewLog(fish) {
+    return fetch(`${config.API_ENDPOINT}/fishing_logs`, {
+      method: "POST",
+      headers: {
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(fish),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  updateLog(fish) {
+    const { fish_id } = fish;
+    return fetch(`${config.API_ENDPOINT}/fishing_logs/${fish_id}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
         "content-type": "application/json",
       },
       body: JSON.stringify(fish),
