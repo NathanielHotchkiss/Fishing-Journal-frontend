f484feb44c9eafbdee9b65f832c2b87709abe7dd
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-09-15 16:53:46 -0400

    removed unnecessary

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -4,45 +4,11 @@ const TokenService = {
   saveAuthToken(token) {
     window.localStorage.setItem(config.TOKEN_KEY, token);
   },
-  setHasVisited() {
-    window.localStorage.setItem("hasVisited", true);
-  },
-  getHasVisited() {
-    return window.localStorage.getItem("hasVisited");
-  },
-  saveEmail(email) {
-    window.localStorage.setItem("email", email);
-  },
-  getEmail() {
-    return window.localStorage.getItem("email");
-  },
-  saveFirstName(first_name) {
-    window.localStorage.setItem("first_name", first_name);
-  },
-  getFirstName() {
-    return window.localStorage.getItem("first_name");
-  },
-  saveLastName(last_name) {
-    window.localStorage.setItem("last_name", last_name);
-  },
-  getLastName() {
-    return window.localStorage.getItem("last_name");
-  },
   saveUserId(user_id) {
     window.localStorage.setItem("user_id", user_id);
   },
   getUserId() {
     return window.localStorage.getItem("user_id");
-  },
-  clearAll() {
-    const items = [
-      "email",
-      "first_name",
-      "last_name",
-      "user_id",
-      config.TOKEN_KEY,
-    ];
-    items.forEach((item) => window.localStorage.removeItem(item));
   },
   clearEverything() {
     window.localStorage.clear();
@@ -52,9 +18,6 @@ const TokenService = {
   },
   hasAuthToken() {
     return !!TokenService.getAuthToken();
-  },
-  makeBasicAuthToken(userName, password) {
-    return window.btoa(`${userName}:${password}`);
   },
 };
 
