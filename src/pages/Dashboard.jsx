27101a57e445b0dd0e8530864edfb48de8720956
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-09-25 22:25:56 -0400

    set up sort component, ui changes to card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,16 @@
 import React, { Fragment, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import { Menu, Transition } from "@headlessui/react";
+import Layout from "../layout/Layout";
+import Sort from "../components/Sort";
+import { UserContext } from "../routes/AppRoutes";
+import AuthApiService from "../services/auth-api-service";
 import {
   TrashIcon,
   PencilSquareIcon,
   PlusIcon,
 } from "@heroicons/react/24/outline";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
-
-import Layout from "../layout/Layout";
-import { UserContext } from "../routes/AppRoutes";
-import AuthApiService from "../services/auth-api-service";
 
 export default function Dashboard() {
   const context = useContext(UserContext);
@@ -51,7 +50,7 @@ export default function Dashboard() {
             />
           </svg>
           <h3 className="mt-2 text-sm font-medium text-gray-900">
-            No logs yet
+            Nothing to show yet
           </h3>
           <p className="mt-1 text-sm text-gray-500">
             Get started by logging a catch.
@@ -72,111 +71,109 @@ export default function Dashboard() {
   } else {
     return (
       <Layout title="Dashboard">
+        <Sort />
         <div className="grid grid-col-1 lg:grid-cols-2 my-6">
-          <>
-            {userLogs.map((info, id) => {
-              const {
-                fish_id,
-                species,
-                fish_length,
-                pounds,
-                ounces,
-                bait,
-                fishing_method,
-              } = info;
+          {userLogs.map((info, id) => {
+            const {
+              fish_id,
+              species,
+              fish_length,
+              pounds,
+              ounces,
+              bait,
+              fishing_method,
+            } = info;
 
-              return (
-                <div key={id} className="block">
-                  <div className="relative h-72 w-96 shadow-md mx-auto bg-zinc-300 "></div>
-
-                  <div className="relative p-2 h-40 w-72 shadow-md -top-12 mx-auto bg-white rounded-lg">
-                    <div className="flex justify-between">
-                      <p className="mt-2 block text-lg capitalize font-medium text-gray-900 truncate pointer-events-none">
-                        {species}
-                      </p>
-
-                      <div>
-                        <Menu
-                          as="div"
-                          className="relative inline-block text-left mt-2"
-                        >
-                          <div>
-                            <Menu.Button className="bg-gray-100 rounded-full flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500">
-                              <span className="sr-only">Open options</span>
-                              <EllipsisVerticalIcon
-                                className="h-5 w-5"
-                                aria-hidden="true"
-                              />
-                            </Menu.Button>
-                          </div>
-
-                          <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                          >
-                            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                              <div className="py-1">
-                                <Menu.Item>
-                                  {({ active }) => (
-                                    <Link
-                                      to={`/fishing_logs/${fish_id}/edit`}
-                                      className={classNames(
-                                        active
-                                          ? "bg-gray-100 text-gray-900"
-                                          : "text-gray-700",
-                                        "group flex items-center px-4 py-2 text-sm"
-                                      )}
-                                    >
-                                      <PencilSquareIcon
-                                        className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                                        aria-hidden="true"
-                                      />
-                                      Edit
-                                    </Link>
-                                  )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                  {({ active }) => (
-                                    <Link
-                                      to="#"
-                                      onClick={() => handleDelete(fish_id)}
-                                      className={classNames(
-                                        active
-                                          ? "w-full bg-gray-100 text-gray-900"
-                                          : "text-gray-700",
-                                        "group flex items-center px-4 py-2 text-sm"
-                                      )}
-                                    >
-                                      <TrashIcon
-                                        className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                                        aria-hidden="true"
-                                      />
-                                      Delete
-                                    </Link>
-                                  )}
-                                </Menu.Item>
-                              </div>
-                            </Menu.Items>
-                          </Transition>
-                        </Menu>
-                      </div>
-                    </div>
-                    <p className="block text-base font-medium text-gray-500 pointer-events-none">
-                      <span>Id: </span>
-                      {fish_id}
-                      <span className="text-xs"></span>
+            return (
+              <div key={id} className="block">
+                <div className="relative h-72 w-96 shadow-md mx-auto bg-zinc-300"></div>
+                <div className="relative px-4 py-2 h-44 w-72 shadow-md -top-12 mx-auto bg-white rounded-lg">
+                  <div className="flex justify-between">
+                    <p className="mt-2 block text-lg capitalize font-semibold text-zinc-600 truncate">
+                      {species}
                     </p>
-                    <p className="block text-base font-medium text-gray-500 pointer-events-none">
+                    <div>
+                      <Menu
+                        as="div"
+                        className="relative inline-block text-left mt-2"
+                      >
+                        <div>
+                          <Menu.Button className="bg-gray-100 rounded-full flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500">
+                            <span className="sr-only">Open options</span>
+                            <EllipsisVerticalIcon
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                            />
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to={`/fishing_logs/${fish_id}/edit`}
+                                    className={classNames(
+                                      active
+                                        ? "bg-gray-100 text-gray-900"
+                                        : "text-gray-700",
+                                      "group flex items-center px-4 py-2 text-sm"
+                                    )}
+                                  >
+                                    <PencilSquareIcon
+                                      className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                                      aria-hidden="true"
+                                    />
+                                    Edit
+                                  </Link>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="#"
+                                    onClick={() => handleDelete(fish_id)}
+                                    className={classNames(
+                                      active
+                                        ? "w-full bg-gray-100 text-gray-900"
+                                        : "text-gray-700",
+                                      "group flex items-center px-4 py-2 text-sm"
+                                    )}
+                                  >
+                                    <TrashIcon
+                                      className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                                      aria-hidden="true"
+                                    />
+                                    Delete
+                                  </Link>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                  </div>
+                  <ul className="block text-base font-medium text-zinc-700">
+                    <li>
+                      <span>id: </span>
+                      {fish_id}
+                    </li>
+                    <li>
                       <span>Length: </span>
                       {fish_length}
                       <span className="text-xs">in</span>
-                    </p>
-                    <p className="block text-base font-medium text-gray-500 pointer-events-none">
+                    </li>
+                    <li>
                       <span>Weight: </span>
                       <span>
                         {pounds}
@@ -186,19 +183,17 @@ export default function Dashboard() {
                         {ounces}
                         <span className="text-xs">oz</span>
                       </span>
-                    </p>
-                    <p className="block text-base font-medium text-gray-500 pointer-events-none">
+                    </li>
+                    <li>
                       <span>Bait used: </span>
                       {bait}
-                    </p>
-                    <p className="block text-base font-medium text-gray-500 pointer-events-none">
-                      Method used: {fishing_method}
-                    </p>
-                  </div>
+                    </li>
+                    <li>Method used: {fishing_method}</li>
+                  </ul>
                 </div>
-              );
-            })}
-          </>
+              </div>
+            );
+          })}
         </div>
       </Layout>
     );
