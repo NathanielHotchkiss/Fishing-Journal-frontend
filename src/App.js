6523223c4536899dfae3d3e3f63e35b476168585
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-09-28 21:03:41 -0400

    moved context to App, added loading state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,70 @@
+import React, { useState } from "react";
 import AppRoutes from "./routes/AppRoutes";
+import config from "./config";
+import TokenService from "./services/token-service";
+
+export const UserContext = React.createContext({});
 
 export default function App() {
-  return <AppRoutes />;
+  const [apiError, setApiError] = useState(null);
+  const [fishingLogsData, setFishingLogsData] = useState([]);
+  const [userInfo, setUserInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleApiCalls() {
+    setIsLoading(true);
+
+    if (TokenService.hasAuthToken()) {
+      const user_id = TokenService.getUserId();
+
+      await Promise.all([
+        fetch(`${config.API_ENDPOINT}/fishing_logs/user/${user_id}`, {
+          headers: {
+            Authorization: `bearer ${TokenService.getAuthToken()}`,
+          },
+        }),
+
+        fetch(`${config.API_ENDPOINT}/app_users/${user_id}`, {
+          headers: {
+            Authorization: `bearer ${TokenService.getAuthToken()}`,
+          },
+        }),
+      ])
+
+        .then(([userLogsRes, userInfoRes]) => {
+          if (!userLogsRes.ok) {
+            return userLogsRes.json().then((e) => Promise.reject(e));
+          }
+
+          if (!userInfoRes.ok) {
+            return userInfoRes.json().then((e) => Promise.reject(e));
+          }
+
+          return Promise.all([userLogsRes.json(), userInfoRes.json()]);
+        })
+
+        .then(([logs, user]) => {
+          setFishingLogsData(logs);
+          setUserInfo(user);
+          setIsLoading(false);
+        })
+
+        .catch(setApiError);
+    }
+  }
+
+  const context = {
+    fishingLogsData: fishingLogsData,
+    setApiError: setApiError,
+    apiError: apiError,
+    handleApiCalls: handleApiCalls,
+    userInfo: userInfo,
+    isLoading: isLoading
+  };
+
+  return (
+    <UserContext.Provider value={context}>
+      <AppRoutes />
+    </UserContext.Provider>
+  );
 }
