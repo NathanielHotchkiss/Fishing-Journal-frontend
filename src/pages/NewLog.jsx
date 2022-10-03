02e961ddaee7e72812d80b4985bf912e8d124ef6
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-10-03 15:01:45 -0400

    updated navigation on cancel button

--- src/pages/NewLog.jsx
+++ src/pages/NewLog.jsx
@@ -227,7 +227,7 @@ export default function NewLog({ edit }) {
               <button
                 className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500 mr-2"
                 type="button"
-                onClick={() => navigate(-1)}
+                onClick={() => navigate("/dashboard")}
               >
                 Cancel
               </button>
