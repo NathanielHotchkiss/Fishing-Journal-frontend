ead36700b51a8144ff6fcc9bb416e7d69ca77d4f
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-10-23 13:33:31 -0400

    ui changes

--- src/pages/UserSettings.jsx
+++ src/pages/UserSettings.jsx
@@ -106,21 +106,7 @@ export default function UserSettings() {
             />
           </div>
 
-          <div className="grid sm:grid-cols-2 gap-2 items-start border-t border-gray-200 pt-3 sm:pt-5">
-            <span className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-              Delete account
-            </span>
-
-            <button
-              className="bg-red-100 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-red-700 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500"
-              type="button"
-              onClick={() => handleDelete()}
-            >
-              Delete
-            </button>
-          </div>
-
-          <div className="flex justify-end pt-5">
+          <div className="flex justify-end pt-3">
             <button
               className="ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500 mr-2"
               type="button"
@@ -135,6 +121,29 @@ export default function UserSettings() {
             >
               Save changes
             </button>
+          </div>
+
+<div className="border-t border-gray-200 my-5 sm:my-8"></div>
+
+          <div className="pt-4">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Delete your account
+            </h3>
+            <div className="mt-2 max-w-xl text-sm text-gray-500">
+              <p>
+                Once you delete your account, you will lose all data associated
+                with it.
+              </p>
+            </div>
+            <div className="mt-3">
+              <button
+                onClick={() => handleDelete()}
+                type="button"
+                className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 font-medium text-red-700 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:text-sm"
+              >
+                Delete account
+              </button>
+            </div>
           </div>
         </div>
       </form>
