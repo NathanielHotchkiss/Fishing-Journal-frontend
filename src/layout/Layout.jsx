7a26a885a4d132adcefcb6cb7b7206fdc4c046da
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-10-23 12:20:07 -0400

    removed user img

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -150,13 +150,6 @@ const Layout = ({ children, title }) => {
                 </div>
                 <div className="flex-shrink-0 flex border-t border-gray-700 p-4">
                   <div className="flex items-center">
-                    <div>
-                      <img
-                        className="inline-block h-10 w-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </div>
                     <div className="ml-3">
                       <p className="text-sm font-medium text-white">{user}</p>
                       <div className="text-sm font-medium text-gray-400">
@@ -244,13 +237,6 @@ const Layout = ({ children, title }) => {
           </div>
           <div className="flex-shrink-0 flex border-t border-gray-700 p-4">
             <div className="flex items-center">
-              <div>
-                <img
-                  className="inline-block h-9 w-9 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </div>
               <div className="ml-3">
                 <p className="text-sm font-medium text-white">{user}</p>
                 <div className="text-sm font-medium text-gray-400">{email}</div>
