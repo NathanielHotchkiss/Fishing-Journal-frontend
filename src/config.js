95e20e1bfbcbb5362f5343e1ac3d906de855ea92
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-10-23 11:18:46 -0400

    vercel

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
-let API_ENDPOINT;
-
-if (process.env.NODE_ENV === "development") {
-  API_ENDPOINT = "http://localhost:5050";
-}
-
-else if (process.env.NODE_ENV === 'production') {
-API_ENDPOINT = 'https://fishing-journal-server.herokuapp.com';
-}
+const API_ENDPOINT =
+  process.env.NODE_ENV === "PRODUCTION"
+    ? "https://fishing-journal-server.herokuapp.com"
+    : "http://localhost:5050";
 
 export default { API_ENDPOINT, TOKEN_KEY: "fishing-client-auth-token" };
